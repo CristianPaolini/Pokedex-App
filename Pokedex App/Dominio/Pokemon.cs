8cbf8f71999619975803f6d11533c9590eb032f6
Cristian Paolini <cristianpaolini3@gmail.com>
2020-09-20 03:06:31 -0300

    Creación de capas, librerías de clases y referencias entre las mismas

---
+++ Pokedex App/Dominio/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Pokemon
+    {
+        private int id;
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImage { get; set; }
+    }
+}
---
+++ Pokedex App/Negocio/PokemonNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class PokemonNegocio
+    {
+        public List<Pokemon> listar()
+        {
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            List < Pokemon > lista = new List<Pokemon>();
+            conexion.ConnectionString = "data source=.\\sqlexpress; initial catalog=POKEMON_DB; integrated security=sspi";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = "Select id, nombre, descripcion, imagen From POKEMONS";
+            comando.Connection = conexion;
+
+            conexion.Open();
+            lector = comando.ExecuteReader();
+            while (lector.Read())
+            {
+                Pokemon aux = new Pokemon();
+                aux.Nombre = lector.GetString(1);
+                aux.Descripcion = lector.GetString(2);
+                aux.UrlImage = lector.GetString(3);
+
+                lista.Add(aux);
+            }
+
+            conexion.Close();
+            return lista;
+
+        }
+    }
+}
