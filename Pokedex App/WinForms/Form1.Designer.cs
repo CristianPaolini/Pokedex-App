fcfffcbb522fa3077b510cdf743557dab2eeff08
Cristian Paolini <cristianpaolini3@gmail.com>
2020-09-20 03:05:02 -0300

    Creación de capas, librerías de clases y referencias entre las mismas

--- Pokedex App/WinForms/Form1.Designer.cs
+++ Pokedex App/WinForms/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinForms
 {
-    partial class Form1
+    partial class Pokedex
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.pbPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(12, 46);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(398, 228);
+            this.dgvLista.TabIndex = 0;
+            this.dgvLista.SelectionChanged += new System.EventHandler(this.dgvLista_SelectionChanged);
+            // 
+            // pbPokemon
+            // 
+            this.pbPokemon.Location = new System.Drawing.Point(439, 46);
+            this.pbPokemon.Name = "pbPokemon";
+            this.pbPokemon.Size = new System.Drawing.Size(336, 228);
+            this.pbPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPokemon.TabIndex = 1;
+            this.pbPokemon.TabStop = false;
+            // 
+            // Pokedex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(787, 325);
+            this.Controls.Add(this.pbPokemon);
+            this.Controls.Add(this.dgvLista);
+            this.Name = "Pokedex";
+            this.Text = "Pokedex";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPokemon)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.PictureBox pbPokemon;
     }
 }
 
--- Pokedex App/WinForms/Form1.cs
+++ Pokedex App/WinForms/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Negocio;
+using Dominio;
 
 namespace WinForms
 {
-    public partial class Form1 : Form
+    public partial class Pokedex : Form
     {
-        public Form1()
+        public Pokedex()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            dgvLista.DataSource = negocio.listar();
+            dgvLista.Columns[2].Visible = false;
+        }
+
+        private void dgvLista_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Pokemon poke = (Pokemon)dgvLista.CurrentRow.DataBoundItem;
+                pbPokemon.Load(poke.UrlImage);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
         }
     }
 }
